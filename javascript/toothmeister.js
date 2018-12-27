0430ad705fe85d66f0d60e7f8eae6e71658e5fd2
Arn R <voodoo15@users.noreply.github.com>
2018-12-27 17:34:16 -0500

    Added function to display profile menu.

--- javascript/toothmeister.js
+++ javascript/toothmeister.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+
+  //ON NAV TOGGLE CLICK, DISPLAY HIDDEN NAV MENU (ON MOBILE)
   $('#nav-toggle').on('click', function(e) {
     e.preventDefault();
 
     $(this).add(".nav-mobile-quick-links").toggleClass('is-open');
-  })
+  });
+
+  //ON PROFILE CLICK, DISPLAY HIDDEN PROFILE MENU
+  $('.nav-profile-name').on('click', function(e) {
+    e.preventDefault();
+
+    $('.nav-profile-menu').toggleClass('is-open');
+    $('.nav-profile-name i').toggleClass('fa-caret-up').toggleClass('fa-caret-down');
+  });
+
 });
